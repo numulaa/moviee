878e34492eb4f7a0f55c50a237556ab6e21f93db
Nurul Mukhlisa <numulaa@gmail.com>
2024-02-08 23:57:31 +0800

    added multiple selection input

--- src/pages/Shuffle.jsx
+++ src/pages/Shuffle.jsx
@@ -1,11 +1,23 @@
 import { onSnapshot, query, where } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
 import React, { useEffect, useState } from "react";
+import { MultiSelect } from "react-multi-select-component";
 import { auth, moviesCollection } from "../firebase";
+import "../styles/Shuffle.css";
+import Input from "../components/Input";
 
 const Shuffle = () => {
   const [movies, setMovies] = useState([]);
   const totalMovies = movies.length;
+
+  const options = [
+    { label: "Grapes 🍇", value: "grapes" },
+    { label: "Mango 🥭", value: "mango" },
+    { label: "Strawberry 🍓", value: "strawberry", disabled: true },
+  ];
+
+  const [selected, setSelected] = useState([]);
+
   const styles = {
     backgroundColor: "red",
     position: "absolute",
@@ -37,7 +49,17 @@ const Shuffle = () => {
   }, []);
   return (
     <div style={mainContainerStyle}>
-      <h1>Shuffle page</h1>
+      <div className="main-shuffle">
+        <h1>Shuffle page</h1>
+        {/* <Input /> */}
+        <MultiSelect
+          options={options}
+          value={selected}
+          onChange={setSelected}
+          labelledBy="Select your friends"
+          className="input-multiselect"
+        />
+      </div>
       {movies.map((movie) => {
         const randomLeftPosition = Math.random(window.innerWidth) * 1000;
         const randomTopPosition = Math.random(window.innerHeight) * 1000;
@@ -50,6 +72,9 @@ const Shuffle = () => {
               position: "absolute",
               left: `${randomLeftPosition}px`,
               top: `${randomTopPosition}px`,
+              // backgroundColor: "red",
+              // padding: "1rem",
+              // borderRadius: "10px",
             }}>
             {movie.title}
           </p>
